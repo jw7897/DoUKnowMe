f1955ad4232b1da86e0013e3ab57e358cd597cf9
root <jw7897@gmail.com>
2021-03-16 17:42:19 +0000

    Started route for new quiz

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+const config = require("./config");
+const Quiz = require("./models/quiz")
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+const mongoose = require('mongoose');
+mongoose.connect(config.db.connection, {useNewUrlParser: true, useUnifiedTopology: true});
 
 
 app.get("/", (req,res) =>{
@@ -13,6 +18,23 @@ app.get("/create", (req,res) =>{
 	res.render("create");
 });
 
+app.post("/create", (req,res) =>{
+	res.render("landing");
+});
+
+// router.post("/", isLoggedIn, async (req, res) => {
+//   const mewQuiz = {
+//     name: req.body.name
+//   }
+
+//   try {
+//     const quiz = await Quiz.create(newQuiz)
+//     res.redirect("/landing");
+//   } catch (err) {
+//     console.log(err);
+//     res.redirect("/landing")
+//   }
+// })
 
 
 app.listen(process.env.PORT || 3000, ()=>{console.log("I don't know you")});
