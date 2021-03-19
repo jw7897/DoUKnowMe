9c6a0ad7025835be43998de9d11038088f92439f
root <jw7897@gmail.com>
2021-03-19 01:28:14 +0000

    Can now post to DB

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const config = require("./config");
 const Quiz = require("./models/quiz")
+const bodyParser = require('body-parser')
+
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 const mongoose = require('mongoose');
 mongoose.connect(config.db.connection, {useNewUrlParser: true, useUnifiedTopology: true});
