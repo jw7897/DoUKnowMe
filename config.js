9510bc52aa96ce39118fc67771f3756a71dbe3e7
Joshua Wallace <70968795+jw7897@users.noreply.github.com>
2021-04-09 13:16:23 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,1 @@
-const config = {
-  db: {
-    username: "admin",
-    connection: "mongodb+srv://jw7897:Rollie77@jwcs.4tmlw.mongodb.net/DoYouKnowMe?retryWrites=true&w=majority"
-  }
-}
-
-
-
-module.exports = config;
+*hidden*
