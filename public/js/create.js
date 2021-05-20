6e64af00b4a1e080e8c0f7f803f01ab8d5f89107
root <jw7897@gmail.com>
2021-05-20 20:30:38 +0000

    Beautify

--- public/js/create.js
+++ public/js/create.js
@@ -1,156 +1,132 @@
 let count = 0;
 
 window.onload = function() {
-document.getElementById("final").style.display = "none"
-document.getElementById("nameinput").style.display = "block"
-document.getElementById("question1input").style.display = "none"
-document.getElementById("nextBtn").onclick = next;
-document.getElementById("backBtn").style.visibility = "hidden";
-document.getElementById("backBtn").onclick = back;
+  document.getElementById("final").style.display = "none"
+  document.getElementById("nameinput").style.display = "block"
+  document.getElementById("question1input").style.display = "none"
+  document.getElementById("nextBtn").onclick = next;
+  document.getElementById("backBtn").style.visibility = "hidden";
+  document.getElementById("backBtn").onclick = back;
 }
 
-function next(){
-	count = count +1;
-	showContent();
+function next() {
+  count = count + 1;
+  showContent();
 }
 
-function back(){
-	count = count -1;
-	showContent();
+function back() {
+  count = count - 1;
+  showContent();
 }
 
-function showContent(){
-	if(count <=0){
-	document.getElementById("backBtn").style.visibility = "hidden";
-	document.getElementById("nameinput").style.display = "block"	
-	document.getElementById("question1input").style.display = "none"
-	}else if(count === 1){
-		document.getElementById("backBtn").style.visibility = "visible";
-	    document.getElementById("nameinput").style.display = "none"
-		document.getElementById("question1input").style.display = "block"
-		document.getElementById("question1RAinput").style.display = "none"
-	} else if(count === 2){
-		document.getElementById("question1input").style.display = "none"
-	    document.getElementById("question1RAinput").style.display = "block"
-		document.getElementById("question1FA1input").style.display = "none"
-	} else if( count === 3){
-		document.getElementById("question1RAinput").style.display = "none"
-	    document.getElementById("question1FA1input").style.display = "block"
-		document.getElementById("question1FA2input").style.display = "none"
-
-	} else if( count === 4){
-		document.getElementById("question1FA1input").style.display = "none"
-	    document.getElementById("question1FA2input").style.display = "block"
-			    document.getElementById("question1FA3input").style.display = "none"
-
-	} else if( count === 5){
-		document.getElementById("question1FA2input").style.display = "none"
-	    document.getElementById("question1FA3input").style.display = "block"
-			    document.getElementById("question2input").style.display = "none"
-
-	} else if( count === 6){
-		document.getElementById("question1FA3input").style.display = "none"
-	    document.getElementById("question2input").style.display = "block"
-			    document.getElementById("question2RAinput").style.display = "none"
-
-	} else if( count === 7){
-		document.getElementById("question2input").style.display = "none"
-	    document.getElementById("question2RAinput").style.display = "block"
-			    document.getElementById("question2FA1input").style.display = "none"
-
-	} else if( count === 8){
-		document.getElementById("question2RAinput").style.display = "none"
-	    document.getElementById("question2FA1input").style.display = "block"
-			    document.getElementById("question2FA2input").style.display = "none"
-
-	} else if( count === 9){
-		document.getElementById("question2FA1input").style.display = "none"
-	    document.getElementById("question2FA2input").style.display = "block"
-			    document.getElementById("question2FA3input").style.display = "none"
-
-	} else if( count === 10){
-		document.getElementById("question2FA2input").style.display = "none"
-	    document.getElementById("question2FA3input").style.display = "block"
-			    document.getElementById("question3input").style.display = "none"
-
-	} else if( count === 11){
-		document.getElementById("question2FA3input").style.display = "none"
-	    document.getElementById("question3input").style.display = "block"
-			    document.getElementById("question3RAinput").style.display = "none"
-
-	} else if( count === 12){
-		document.getElementById("question3input").style.display = "none"
-	    document.getElementById("question3RAinput").style.display = "block"
-			    document.getElementById("question3FA1input").style.display = "none"
-
-	} else if( count === 13){
-		document.getElementById("question3RAinput").style.display = "none"
-	    document.getElementById("question3FA1input").style.display = "block"
-			    document.getElementById("question3FA2input").style.display = "none"
-
-	} else if( count === 14){
-		document.getElementById("question3FA1input").style.display = "none"
-	    document.getElementById("question3FA2input").style.display = "block"
-			    document.getElementById("question3FA3input").style.display = "none"
-
-	} else if( count === 15){
-		document.getElementById("question3FA2input").style.display = "none"
-	    document.getElementById("question3FA3input").style.display = "block"
-			    document.getElementById("question4input").style.display = "none"
-
-	} else if( count === 16){
-		document.getElementById("question3FA3input").style.display = "none"
-	    document.getElementById("question4input").style.display = "block"
-			    document.getElementById("question4RAinput").style.display = "none"
-
-	} else if( count === 17){
-		document.getElementById("question4input").style.display = "none"
-	    document.getElementById("question4RAinput").style.display = "block"
-			    document.getElementById("question4FA1input").style.display = "none"
-
-	} else if( count === 18){
-		document.getElementById("question4RAinput").style.display = "none"
-	    document.getElementById("question4FA1input").style.display = "block"
-			    document.getElementById("question4FA2input").style.display = "none"
-
-	} else if( count === 19){
-		document.getElementById("question4FA1input").style.display = "none"
-	    document.getElementById("question4FA2input").style.display = "block"
-			    document.getElementById("question4FA3input").style.display = "none"
-
-	} else if( count === 20){
-		document.getElementById("question4FA2input").style.display = "none"
-	    document.getElementById("question4FA3input").style.display = "block"
-			    document.getElementById("question5input").style.display = "none"
-
-	} else if( count === 21){
-		document.getElementById("question4FA3input").style.display = "none"
-	    document.getElementById("question5input").style.display = "block"
-			    document.getElementById("question5RAinput").style.display = "none"
-
-	} else if( count === 22){
-		document.getElementById("question5input").style.display = "none"
-	    document.getElementById("question5RAinput").style.display = "block"
-			    document.getElementById("question5FA1input").style.display = "none"
-
-	} else if( count === 23){
-		document.getElementById("question5RAinput").style.display = "none"
-	    document.getElementById("question5FA1input").style.display = "block"
-			    document.getElementById("question5FA2input").style.display = "none"
-
-	} else if( count === 24){
-		document.getElementById("question5FA1input").style.display = "none"
-	    document.getElementById("question5FA2input").style.display = "block"
-			  document.getElementById("question5FA3input").style.display = "none"
-				document.getElementById("nextBtn").style.visibility = "visible";
-				document.getElementById("final").style.display = "none";
-
-
-
-	} else if( count === 25){
-		document.getElementById("question5FA2input").style.display = "none"
-	    document.getElementById("question5FA3input").style.display = "block"
-		document.getElementById("nextBtn").style.visibility = "hidden";
-		document.getElementById("final").style.display = "block";
-	}
+function showContent() {
+  if (count <= 0) {
+    document.getElementById("backBtn").style.visibility = "hidden";
+    document.getElementById("nameinput").style.display = "block"
+    document.getElementById("question1input").style.display = "none"
+  } else if (count === 1) {
+    document.getElementById("backBtn").style.visibility = "visible";
+    document.getElementById("nameinput").style.display = "none"
+    document.getElementById("question1input").style.display = "block"
+    document.getElementById("question1RAinput").style.display = "none"
+  } else if (count === 2) {
+    document.getElementById("question1input").style.display = "none"
+    document.getElementById("question1RAinput").style.display = "block"
+    document.getElementById("question1FA1input").style.display = "none"
+  } else if (count === 3) {
+    document.getElementById("question1RAinput").style.display = "none"
+    document.getElementById("question1FA1input").style.display = "block"
+    document.getElementById("question1FA2input").style.display = "none"
+  } else if (count === 4) {
+    document.getElementById("question1FA1input").style.display = "none"
+    document.getElementById("question1FA2input").style.display = "block"
+    document.getElementById("question1FA3input").style.display = "none"
+  } else if (count === 5) {
+    document.getElementById("question1FA2input").style.display = "none"
+    document.getElementById("question1FA3input").style.display = "block"
+    document.getElementById("question2input").style.display = "none"
+  } else if (count === 6) {
+    document.getElementById("question1FA3input").style.display = "none"
+    document.getElementById("question2input").style.display = "block"
+    document.getElementById("question2RAinput").style.display = "none"
+  } else if (count === 7) {
+    document.getElementById("question2input").style.display = "none"
+    document.getElementById("question2RAinput").style.display = "block"
+    document.getElementById("question2FA1input").style.display = "none"
+  } else if (count === 8) {
+    document.getElementById("question2RAinput").style.display = "none"
+    document.getElementById("question2FA1input").style.display = "block"
+    document.getElementById("question2FA2input").style.display = "none"
+  } else if (count === 9) {
+    document.getElementById("question2FA1input").style.display = "none"
+    document.getElementById("question2FA2input").style.display = "block"
+    document.getElementById("question2FA3input").style.display = "none"
+  } else if (count === 10) {
+    document.getElementById("question2FA2input").style.display = "none"
+    document.getElementById("question2FA3input").style.display = "block"
+    document.getElementById("question3input").style.display = "none"
+  } else if (count === 11) {
+    document.getElementById("question2FA3input").style.display = "none"
+    document.getElementById("question3input").style.display = "block"
+    document.getElementById("question3RAinput").style.display = "none"
+  } else if (count === 12) {
+    document.getElementById("question3input").style.display = "none"
+    document.getElementById("question3RAinput").style.display = "block"
+    document.getElementById("question3FA1input").style.display = "none"
+  } else if (count === 13) {
+    document.getElementById("question3RAinput").style.display = "none"
+    document.getElementById("question3FA1input").style.display = "block"
+    document.getElementById("question3FA2input").style.display = "none"
+  } else if (count === 14) {
+    document.getElementById("question3FA1input").style.display = "none"
+    document.getElementById("question3FA2input").style.display = "block"
+    document.getElementById("question3FA3input").style.display = "none"
+  } else if (count === 15) {
+    document.getElementById("question3FA2input").style.display = "none"
+    document.getElementById("question3FA3input").style.display = "block"
+    document.getElementById("question4input").style.display = "none"
+  } else if (count === 16) {
+    document.getElementById("question3FA3input").style.display = "none"
+    document.getElementById("question4input").style.display = "block"
+    document.getElementById("question4RAinput").style.display = "none"
+  } else if (count === 17) {
+    document.getElementById("question4input").style.display = "none"
+    document.getElementById("question4RAinput").style.display = "block"
+    document.getElementById("question4FA1input").style.display = "none"
+  } else if (count === 18) {
+    document.getElementById("question4RAinput").style.display = "none"
+    document.getElementById("question4FA1input").style.display = "block"
+    document.getElementById("question4FA2input").style.display = "none"
+  } else if (count === 19) {
+    document.getElementById("question4FA1input").style.display = "none"
+    document.getElementById("question4FA2input").style.display = "block"
+    document.getElementById("question4FA3input").style.display = "none"
+  } else if (count === 20) {
+    document.getElementById("question4FA2input").style.display = "none"
+    document.getElementById("question4FA3input").style.display = "block"
+    document.getElementById("question5input").style.display = "none"
+  } else if (count === 21) {
+    document.getElementById("question4FA3input").style.display = "none"
+    document.getElementById("question5input").style.display = "block"
+    document.getElementById("question5RAinput").style.display = "none"
+  } else if (count === 22) {
+    document.getElementById("question5input").style.display = "none"
+    document.getElementById("question5RAinput").style.display = "block"
+    document.getElementById("question5FA1input").style.display = "none"
+  } else if (count === 23) {
+    document.getElementById("question5RAinput").style.display = "none"
+    document.getElementById("question5FA1input").style.display = "block"
+    document.getElementById("question5FA2input").style.display = "none"
+  } else if (count === 24) {
+    document.getElementById("question5FA1input").style.display = "none"
+    document.getElementById("question5FA2input").style.display = "block"
+    document.getElementById("question5FA3input").style.display = "none"
+    document.getElementById("nextBtn").style.visibility = "visible";
+    document.getElementById("final").style.display = "none";
+  } else if (count === 25) {
+    document.getElementById("question5FA2input").style.display = "none"
+    document.getElementById("question5FA3input").style.display = "block"
+    document.getElementById("nextBtn").style.visibility = "hidden";
+    document.getElementById("final").style.display = "block";
+  }
 };
--- public/js/take.js
+++ public/js/take.js
@@ -3,24 +3,18 @@ window.onload = function() {
   document.getElementById("nextBtn").onclick = next;
 }
 
-function next(){
-	count = count +1;
-	showContent();
-}
-
-
-function next() {	
+function next() {
   count = count + 1;
   if (count === 1) {
-	let q1 = document.getElementById("q1").textContent
+    let q1 = document.getElementById("q1").textContent
     let ra = document.getElementById("question1RAinpt").value
     let fa1 = document.getElementById("question1FA1inpt").value
     let fa2 = document.getElementById("question1FA2inpt").value
     let fa3 = document.getElementById("question1FA3inpt").value
-    let arr = ['<h2 id="q1"><%=quiz.question1%></h2>','<input type="radio" name="q1answBtns" value="<%=quiz.question1RA%>"><label for="question1RA" id="question1RA"><%=quiz.question1RA%></label><br>','<input type="radio" name="q1answBtns" value="<%=quiz.question1FA1%>"><label for="question1FA1" id="question1FA1"><%=quiz.question1FA1%></label><br>','<input type="radio" name="q1answBtns" value="<%=quiz.question1FA2%>"><label for="question1FA2" id="question1FA2"><%=quiz.question1FA1%></label><br>',' <input type="radio" name="q1answBtns" value="<%=quiz.question1FA3%>"><label for="question1FA3" id="question1FA3"><%=quiz.question1FA3%></label><br>'];
+    let arr = ['<h2 id="q1"><%=quiz.question1%></h2>', '<input type="radio" name="q1answBtns" value="<%=quiz.question1RA%>"><label for="question1RA" id="question1RA"><%=quiz.question1RA%></label><br>', '<input type="radio" name="q1answBtns" value="<%=quiz.question1FA1%>"><label for="question1FA1" id="question1FA1"><%=quiz.question1FA1%></label><br>', '<input type="radio" name="q1answBtns" value="<%=quiz.question1FA2%>"><label for="question1FA2" id="question1FA2"><%=quiz.question1FA1%></label><br>', ' <input type="radio" name="q1answBtns" value="<%=quiz.question1FA3%>"><label for="question1FA3" id="question1FA3"><%=quiz.question1FA3%></label><br>'];
     document.getElementById("nameinput").style.display = "none"
     document.getElementById("question1").innerHTML = arr.shift() + arr.splice(Math.floor(Math.random() * arr.length), 1)[0] + arr.splice(Math.floor(Math.random() * arr.length), 1)[0] + arr.splice(Math.floor(Math.random() * arr.length), 1)[0] + arr.splice(Math.floor(Math.random() * arr.length), 1)[0];
-	document.getElementById("q1").innerHTML = q1;
+    document.getElementById("q1").innerHTML = q1;
     document.getElementById("question1RA").innerHTML = ra;
     document.getElementById("question1FA1").innerHTML = fa1;
     document.getElementById("question1FA2").innerHTML = fa2;
@@ -28,15 +22,15 @@ function next() {
     document.getElementById("question1").style.display = "block"
   } else if (count === 2) {
     document.getElementById("question1").style.display = "none"
-let q2 = document.getElementById("q2").textContent
+    let q2 = document.getElementById("q2").textContent
     let ra = document.getElementById("question2RAinpt").value
     let fa1 = document.getElementById("question2FA1inpt").value
     let fa2 = document.getElementById("question2FA2inpt").value
     let fa3 = document.getElementById("question2FA3inpt").value
-    let arr = ['<h2 id="q2"><%=quiz.question2%></h2>','<input type="radio" name="q2answBtns" value="<%=quiz.question2RA%>"><label for="question2RA" id="question2RA"><%=quiz.question2RA%></label><br>','<input type="radio" name="q2answBtns" value="<%=quiz.question2FA1%>"><label for="question2FA1" id="question2FA1"><%=quiz.question2FA1%></label><br>','<input type="radio" name="q2answBtns" value="<%=quiz.question2FA2%>"><label for="question2FA2" id="question2FA2"><%=quiz.question2FA1%></label><br>',' <input type="radio" name="q2answBtns" value="<%=quiz.question2FA3%>"><label for="question2FA3" id="question2FA3"><%=quiz.question2FA3%></label><br>'];
+    let arr = ['<h2 id="q2"><%=quiz.question2%></h2>', '<input type="radio" name="q2answBtns" value="<%=quiz.question2RA%>"><label for="question2RA" id="question2RA"><%=quiz.question2RA%></label><br>', '<input type="radio" name="q2answBtns" value="<%=quiz.question2FA1%>"><label for="question2FA1" id="question2FA1"><%=quiz.question2FA1%></label><br>', '<input type="radio" name="q2answBtns" value="<%=quiz.question2FA2%>"><label for="question2FA2" id="question2FA2"><%=quiz.question2FA1%></label><br>', ' <input type="radio" name="q2answBtns" value="<%=quiz.question2FA3%>"><label for="question2FA3" id="question2FA3"><%=quiz.question2FA3%></label><br>'];
     document.getElementById("nameinput").style.display = "none"
     document.getElementById("question2").innerHTML = arr.shift() + arr.splice(Math.floor(Math.random() * arr.length), 1)[0] + arr.splice(Math.floor(Math.random() * arr.length), 1)[0] + arr.splice(Math.floor(Math.random() * arr.length), 1)[0] + arr.splice(Math.floor(Math.random() * arr.length), 1)[0];
-	document.getElementById("q2").innerHTML = q2;
+    document.getElementById("q2").innerHTML = q2;
     document.getElementById("question2RA").innerHTML = ra;
     document.getElementById("question2FA1").innerHTML = fa1;
     document.getElementById("question2FA2").innerHTML = fa2;
@@ -44,15 +38,15 @@ let q2 = document.getElementById("q2").textContent
     document.getElementById("question2").style.display = "block"
   } else if (count === 3) {
     document.getElementById("question2").style.display = "none"
-let q3 = document.getElementById("q3").textContent
+    let q3 = document.getElementById("q3").textContent
     let ra = document.getElementById("question3RAinpt").value
     let fa1 = document.getElementById("question3FA1inpt").value
     let fa2 = document.getElementById("question3FA2inpt").value
     let fa3 = document.getElementById("question3FA3inpt").value
-    let arr = ['<h2 id="q3"><%=quiz.question3%></h2>','<input type="radio" name="q3answBtns" value="<%=quiz.question3RA%>"><label for="question3RA" id="question3RA"><%=quiz.question3RA%></label><br>','<input type="radio" name="q3answBtns" value="<%=quiz.question3FA1%>"><label for="question3FA1" id="question3FA1"><%=quiz.question3FA1%></label><br>','<input type="radio" name="q3answBtns" value="<%=quiz.question3FA2%>"><label for="question3FA2" id="question3FA2"><%=quiz.question3FA1%></label><br>',' <input type="radio" name="q3answBtns" value="<%=quiz.question3FA3%>"><label for="question3FA3" id="question3FA3"><%=quiz.question3FA3%></label><br>'];
+    let arr = ['<h2 id="q3"><%=quiz.question3%></h2>', '<input type="radio" name="q3answBtns" value="<%=quiz.question3RA%>"><label for="question3RA" id="question3RA"><%=quiz.question3RA%></label><br>', '<input type="radio" name="q3answBtns" value="<%=quiz.question3FA1%>"><label for="question3FA1" id="question3FA1"><%=quiz.question3FA1%></label><br>', '<input type="radio" name="q3answBtns" value="<%=quiz.question3FA2%>"><label for="question3FA2" id="question3FA2"><%=quiz.question3FA1%></label><br>', ' <input type="radio" name="q3answBtns" value="<%=quiz.question3FA3%>"><label for="question3FA3" id="question3FA3"><%=quiz.question3FA3%></label><br>'];
     document.getElementById("nameinput").style.display = "none"
     document.getElementById("question3").innerHTML = arr.shift() + arr.splice(Math.floor(Math.random() * arr.length), 1)[0] + arr.splice(Math.floor(Math.random() * arr.length), 1)[0] + arr.splice(Math.floor(Math.random() * arr.length), 1)[0] + arr.splice(Math.floor(Math.random() * arr.length), 1)[0];
-	document.getElementById("q3").innerHTML = q3;
+    document.getElementById("q3").innerHTML = q3;
     document.getElementById("question3RA").innerHTML = ra;
     document.getElementById("question3FA1").innerHTML = fa1;
     document.getElementById("question3FA2").innerHTML = fa2;
@@ -60,15 +54,15 @@ let q3 = document.getElementById("q3").textContent
     document.getElementById("question3").style.display = "block"
   } else if (count === 4) {
     document.getElementById("question3").style.display = "none"
-let q4 = document.getElementById("q4").textContent
+    let q4 = document.getElementById("q4").textContent
     let ra = document.getElementById("question4RAinpt").value
     let fa1 = document.getElementById("question4FA1inpt").value
     let fa2 = document.getElementById("question4FA2inpt").value
     let fa3 = document.getElementById("question4FA3inpt").value
-    let arr = ['<h2 id="q4"><%=quiz.question4%></h2>','<input type="radio" name="q4answBtns" value="<%=quiz.question4RA%>"><label for="question4RA" id="question4RA"><%=quiz.question4RA%></label><br>','<input type="radio" name="q4answBtns" value="<%=quiz.question4FA1%>"><label for="question4FA1" id="question4FA1"><%=quiz.question4FA1%></label><br>','<input type="radio" name="q4answBtns" value="<%=quiz.question4FA2%>"><label for="question4FA2" id="question4FA2"><%=quiz.question4FA1%></label><br>',' <input type="radio" name="q4answBtns" value="<%=quiz.question4FA3%>"><label for="question4FA3" id="question4FA3"><%=quiz.question4FA3%></label><br>'];
+    let arr = ['<h2 id="q4"><%=quiz.question4%></h2>', '<input type="radio" name="q4answBtns" value="<%=quiz.question4RA%>"><label for="question4RA" id="question4RA"><%=quiz.question4RA%></label><br>', '<input type="radio" name="q4answBtns" value="<%=quiz.question4FA1%>"><label for="question4FA1" id="question4FA1"><%=quiz.question4FA1%></label><br>', '<input type="radio" name="q4answBtns" value="<%=quiz.question4FA2%>"><label for="question4FA2" id="question4FA2"><%=quiz.question4FA1%></label><br>', ' <input type="radio" name="q4answBtns" value="<%=quiz.question4FA3%>"><label for="question4FA3" id="question4FA3"><%=quiz.question4FA3%></label><br>'];
     document.getElementById("nameinput").style.display = "none"
     document.getElementById("question4").innerHTML = arr.shift() + arr.splice(Math.floor(Math.random() * arr.length), 1)[0] + arr.splice(Math.floor(Math.random() * arr.length), 1)[0] + arr.splice(Math.floor(Math.random() * arr.length), 1)[0] + arr.splice(Math.floor(Math.random() * arr.length), 1)[0];
-	document.getElementById("q4").innerHTML = q4;
+    document.getElementById("q4").innerHTML = q4;
     document.getElementById("question4RA").innerHTML = ra;
     document.getElementById("question4FA1").innerHTML = fa1;
     document.getElementById("question4FA2").innerHTML = fa2;
@@ -81,10 +75,10 @@ let q4 = document.getElementById("q4").textContent
     let fa1 = document.getElementById("question5FA1inpt").value
     let fa2 = document.getElementById("question5FA2inpt").value
     let fa3 = document.getElementById("question5FA3inpt").value
-    let arr = ['<h2 id="q5"><%=quiz.question5%></h2>','<input type="radio" name="q5answBtns" value="<%=quiz.question5RA%>"><label for="question5RA" id="question5RA"><%=quiz.question5RA%></label><br>','<input type="radio" name="q5answBtns" value="<%=quiz.question5FA1%>"><label for="question5FA1" id="question5FA1"><%=quiz.question5FA1%></label><br>','<input type="radio" name="q5answBtns" value="<%=quiz.question5FA2%>"><label for="question5FA2" id="question5FA2"><%=quiz.question5FA1%></label><br>',' <input type="radio" name="q5answBtns" value="<%=quiz.question5FA3%>"><label for="question5FA3" id="question5FA3"><%=quiz.question5FA3%></label><br>'];
+    let arr = ['<h2 id="q5"><%=quiz.question5%></h2>', '<input type="radio" name="q5answBtns" value="<%=quiz.question5RA%>"><label for="question5RA" id="question5RA"><%=quiz.question5RA%></label><br>', '<input type="radio" name="q5answBtns" value="<%=quiz.question5FA1%>"><label for="question5FA1" id="question5FA1"><%=quiz.question5FA1%></label><br>', '<input type="radio" name="q5answBtns" value="<%=quiz.question5FA2%>"><label for="question5FA2" id="question5FA2"><%=quiz.question5FA1%></label><br>', ' <input type="radio" name="q5answBtns" value="<%=quiz.question5FA3%>"><label for="question5FA3" id="question5FA3"><%=quiz.question5FA3%></label><br>'];
     document.getElementById("nameinput").style.display = "none"
     document.getElementById("question5").innerHTML = arr.shift() + arr.splice(Math.floor(Math.random() * arr.length), 1)[0] + arr.splice(Math.floor(Math.random() * arr.length), 1)[0] + arr.splice(Math.floor(Math.random() * arr.length), 1)[0] + arr.splice(Math.floor(Math.random() * arr.length), 1)[0];
-	document.getElementById("q5").innerHTML = q5;
+    document.getElementById("q5").innerHTML = q5;
     document.getElementById("question5RA").innerHTML = ra;
     document.getElementById("question5FA1").innerHTML = fa1;
     document.getElementById("question5FA2").innerHTML = fa2;
@@ -94,4 +88,3 @@ let q4 = document.getElementById("q4").textContent
     document.getElementById("final").style.display = "block";
   }
 };
-
